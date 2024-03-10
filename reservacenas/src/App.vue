90cec033910d3c3674245c9eaf1ea5017c4c3c7c
NextCodigos <nebulanemesis372@gmail.com>
2024-03-11 00:46:32 +0100

    Aplicacion 1 cenas con reyes visigodo

---
+++ reservacenas/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+ 
+</template>
+
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+// Importamos valores de archivo desctrusturamos para acceder datos
+import { productos } from './datos.js';
+</script>
+
+
+<style scoped>
+
+</style>
